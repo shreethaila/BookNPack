966f9d34f54642dfc997bac356c56358c152a643
shree.thaila <shree.thaila@gmail.com>
2023-06-05 05:41:43 +0530

    env

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const {createPool}=require("mysql2")
 const pool=createPool({
     port: process.env.DB_PORT,
-    host:process.env.HOST,
+    host:process.env.DB_HOST,
     user:process.env.USER,
     password:process.env.PASS,
     database:process.env.DB,
