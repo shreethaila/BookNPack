059e36f623138540693bfa4e22c56125e38c8eb5
shree.thaila <shree.thaila@gmail.com>
2023-06-05 07:43:28 +0530

    domain

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Pragma', 'no-cache');
   res.setHeader('Expires', '0');
-  res.setHeader('Access-Control-Allow-Origin', '*')
   next();
 });
 
